cf3e5c66639151d0991936421fd7e61fb135961b
Anastasiia Vozniuk <a.vozniuk.97@gmail.com>
2022-07-12 18:36:43 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+// * функція створення розмітки
+function createImageMarkup({ preview, original, description }) {
+  return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+          `
+};
+
+// * створюється розмітка всієї галерея
+const createGalleryMarkup = galleryItems
+  .map(createImageMarkup)
+  .join('');
+
+// * додається розмітка галереї на сторінку в головний div.gallery
+gallery.insertAdjacentHTML("beforeend", createGalleryMarkup);
+
+let gallerySL = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
