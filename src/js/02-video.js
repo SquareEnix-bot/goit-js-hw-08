1c251319c1e3e5628419894bedb1c2be6be762f4
Anastasiia Vozniuk <a.vozniuk.97@gmail.com>
2022-07-15 01:10:20 +0300

    task-2 DONE

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', _throttle(timeUpdate, 1000));
+
+// * ця функція отримує секунду програвання
+function timeUpdate(data) {  
+  localStorage.setItem('videoplayer-current-time', data.seconds);// * зберігає у сховище поточну секунду
+}
+
+const currentTime = localStorage.getItem('videoplayer-current-time');// * виймає зі сховища поточну секунду
+
+player.setCurrentTime(currentTime).then(function(seconds) { // * воно працює
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
