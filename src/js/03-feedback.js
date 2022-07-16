e67a975a808db61217fa47dd7b399f21c666af8c
Anastasiia Vozniuk <a.vozniuk.97@gmail.com>
2022-07-16 23:12:24 +0300

    add comments

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,42 +5,65 @@ const button = document.querySelector('button[type="submit"]');
 
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
+// ? запитати в ментора про перевірку на поля
+// TODO зробити нормальну перевірку
 if (form.elements.email.value === "" && form.elements.message.value === "") {
   button.disabled = true;
 } 
 
+const formFieldsData = {
+  email: "",
+  message: "",
+};
+
 form.addEventListener('input', _throttle(onFormInput, 500));
+
 form.addEventListener('submit', onFormSubmit);
 
 formFilling();
 
-function onFormInput(event) {
+// * спосію з об'єктом всередині (сама придумала)
+// function onFormInput(event) {
 
-  const formData = {
-    email: form.elements.email.value,
-    message: form.elements.message.value,
-  };   
+//   const formData = {
+//     email: form.elements.email.value,
+//     message: form.elements.message.value,
+//   };
   
+//   if (form.elements.email.value != "" && form.elements.message.value != "") {
+//     button.disabled = false;
+//   } else {
+//     button.disabled = true;
+//   }
+
+//   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+// };
+
+// * спосію з об'єктом зовні (Репета)
+// ? іноді вискакує undefined, запитати в ментора
+function onFormInput(event) {
+  formFieldsData[event.target.name] = event.target.value;
+
   if (form.elements.email.value != "" && form.elements.message.value != "") {
     button.disabled = false;
   } else {
     button.disabled = true;
   }  
 
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-};
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formFieldsData));
+}
 
 function onFormSubmit(event) {
-  event.preventDefault();  
+  event.preventDefault();
 
   console.log('Entered Data: ', parseLocalData());
 
   event.currentTarget.reset(); // * очищає поля форми
-  localStorage.removeItem(LOCALSTORAGE_KEY); // * очищає локальне сховище  
+  localStorage.removeItem(LOCALSTORAGE_KEY); // * очищає локальне сховище
 
   if (form.elements.email.value === "" && form.elements.message.value === "") {
     button.disabled = true;
-  } 
+  }
 };
 
 function formFilling() { // * виймає дані з локального сховища та ставить у форму при оновленні
@@ -53,10 +76,9 @@ function formFilling() { // * виймає дані з локального сх
 };
 
 // * виймає об'єкт з локального сховища
-function parseLocalData() {  
+function parseLocalData() {
   const savedFormData = localStorage.getItem(LOCALSTORAGE_KEY);
   const parsedFormData = JSON.parse(savedFormData);
 
   return JSON.parse(savedFormData);
 }
-
